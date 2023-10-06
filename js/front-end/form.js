84d028073c93d16d829940720f56db83d3e98ef6
Jose Carlos Garcia <Carlos.cg895@live.com>
2023-10-06 09:39:40 -0500

    further project cleanup

--- js/front-end/form.js
+++ js/front-end/form.js
@@ -6,9 +6,31 @@
  * Last Modified: 9/21/2023
  */
 
+// Document Elements 
+const form = document.getElementById('check-in-form');
+const currentTimeElement = document.getElementById('current-time');
+const switchButton = document.getElementById('signin-out-button');
+
+// Main Fieldset Inputs 
+const jobLabel = document.getElementById('job-label');
+const jobSelect = document.getElementById('job');
+const visitPurposeFieldset = document.getElementById('visit-purpose');
+const contractPurposeFieldset = document.getElementById('contractor-purpose');
+
+// Contractor Field Variable Inputs 
+const contractor_purpose = document.getElementById('contractor-reason');
+const contractor_company = document.getElementById('contractor-company');
+const contractor_contact = document.getElementById('contractor-contact');
+const namesContainer = document.getElementById('names');
+const nameEntries = namesContainer.querySelectorAll('.name-entry');
+const newNameEntry = document.createElement('div');
+
+//Visitor Field Variable Inputs
+const visitor_purpose = document.getElementById('visit-reason');
+
+
 // Function to update and display the current time
 function updateCurrentTime() {
-    const currentTimeElement = document.getElementById('current-time');
     const now = new Date();
   
     // Get hours, minutes, and seconds
@@ -28,10 +50,7 @@ function updateCurrentTime() {
 
   // Function to show or hide the visit purpose fieldset based on the selected visitor type
 function toggleVisitPurpose() {
-    const jobSelect = document.getElementById('job');
-    const visitPurposeFieldset = document.getElementById('visit-purpose');
-    const contractPurposeFieldset = document.getElementById('contractor-purpose');
-     
+
      if (jobSelect.value === 'Visitor: Customer' || jobSelect.value === 'Visitor: Partner' || jobSelect.value === 'Visitor: Other'  ){
         setContractorFields(false);
         setVisitorFields(true)
@@ -53,10 +72,6 @@ function toggleVisitPurpose() {
     }
 
     function setContractorFields(viewOption) {
-      const contractor_purpose = document.getElementById('contractor-reason');
-      const contractor_company = document.getElementById('contractor-company');
-      const contractor_contact = document.getElementById('contractor-contact');
-    
       contractor_purpose.required = viewOption;
       contractor_company.required = viewOption;
       contractor_contact.required = viewOption;
@@ -64,41 +79,36 @@ function toggleVisitPurpose() {
     
     // Function to show or hide the visitor purpose fieldset based on the selected visitor type
     function setVisitorFields(viewOption) {
-      const visitor_purpose = document.getElementById('visit-reason');
       visitor_purpose.required = viewOption;
     }
 // Add an event listener to the job select element to call the function when the selection changes
-document.getElementById('job').addEventListener('change', toggleVisitPurpose);
+jobSelect.addEventListener('change', toggleVisitPurpose);
         
 // Call the function initially to set the initial state
 toggleVisitPurpose();
 
 // Function to toggle the sign-in/out form type
 function toggleFormType(){
-  const switchButton = document.getElementById('signin-out-button');
-  const jobDropDown = document.getElementById('job');
-  const jobLabel = document.getElementById('job-label');
-
   // default to sign-in form
   if (switchButton.textContent === 'Sign-In') {
-    jobDropDown.required = true; 
-    jobDropDown.style.display = 'block';
+    jobSelect.required = true; 
+    jobSelect.style.display = 'block';
     jobLabel.style.display = 'block'; 
   } else {
-    jobDropDown.required = false;
-    jobDropDown.style.display = 'none';
+    jobSelect.required = false;
+    jobSelect.style.display = 'none';
     jobLabel.style.display = 'none'; 
   }
   // Add an event listener to the button to toggle its text between "Sign-In" and "Sign-Out"
   switchButton.addEventListener('click', () => {
     if (switchButton.textContent === 'Sign-In') {
-      jobDropDown.required = false;
-      jobDropDown.style.display = 'none';
+      jobSelect.required = false;
+      jobSelect.style.display = 'none';
       jobLabel.style.display = 'none'; 
       switchButton.textContent = 'Sign-Out';
     } else {
-      jobDropDown.required = true;
-      jobDropDown.style.display = 'block';
+      jobSelect.required = true;
+      jobSelect.style.display = 'block';
       jobLabel.style.display = 'block'; 
       switchButton.textContent = 'Sign-In';
     }
@@ -110,9 +120,6 @@ toggleFormType();
 
 // Function to add a new name entry with a delete button
 function addName() {
-  const namesContainer = document.getElementById('names');
-  const nameEntries = namesContainer.querySelectorAll('.name-entry');
-  const newNameEntry = document.createElement('div');
   newNameEntry.className = 'name-entry';
 
   // Clone the HTML for the name field and delete button
@@ -130,6 +137,8 @@ function addName() {
   deleteButton.addEventListener('click', () => {
     namesContainer.removeChild(newNameEntry);
     updateDeleteButtons();
+    // Set the "required" attribute of the name input field to false
+    newNameEntry.querySelector('input').required = false; 
   });
 
   // Add an event listener for input validation
@@ -142,9 +151,27 @@ function addName() {
   // Append the new name entry to the container
   namesContainer.appendChild(newNameEntry);
 
+  // Set the "required" attribute of the name input field to true 
+  nameInput.required = true; 
+
   // Update delete buttons
   updateDeleteButtons();
 }
+
+
+// Set the button's position and width based on the form's position and width
+function updateButtonPosition() {
+  const formRect = form.getBoundingClientRect();
+
+  switchButton.style.width = `${formRect.width}px`;
+  switchButton.style.bottom = `${window.innerHeight - formRect.bottom + 10}px`;
+}
+
+// Add an event listener for window resize events
+window.addEventListener('resize', updateButtonPosition);
+
+// Call updateButtonPosition initially to set the initial state
+updateButtonPosition();
 
 // Function to update the visibility of delete buttons
 function updateDeleteButtons() {
--- js/front-end/format.js
+++ js/front-end/format.js
@@ -6,9 +6,23 @@
  * Last Modified: 9/21/2023
  */
 
+// Main Form Inputs 
+const nameInput = document.getElementById('user_name');
+const phoneNumberInput = document.getElementById('tel');
+
+// Visitor Fields
+const visitReasonInput = document.getElementById('visit-reason');
+const formatVisitCompanyInput = document.getElementById('visit-company');
+
+// Contractor Fields
+const contractorReasonInput = document.getElementById('contractor-reason');
+const contractorCompanyInput = document.getElementById('contractor-company');
+const contractorContactInput = document.getElementById('contractor-contact');
+const contractorAreaInput = document.getElementById('contractor-area');
+
   // Function to format the phone number as the user types
   function formatPhoneNumber() {
-    const phoneNumberInput = document.getElementById('tel');
+
     let phoneNumber = phoneNumberInput.value.replace(/\D/g, ''); // Remove non-numeric characters
     
     if (phoneNumber.length >= 4) {
@@ -20,60 +34,46 @@
     // Cap the phone number length at 12 characters (including dashes)
     phoneNumber = phoneNumber.substring(0, 12);  
     phoneNumberInput.value = phoneNumber; // Update the input field with the formatted and capped phone number
-  }document.getElementById('tel').addEventListener('input', formatPhoneNumber);
+  }phoneNumberInput.addEventListener('input', formatPhoneNumber);
   
   // Function to formata the name input
   function formatName(){
-    const nameInput = document.getElementById('user_name');
     let name = nameInput.value.replace(/[^a-zA-Z ]/g, ""); // Remove non-alphabetic characters
     nameInput.value = name; // Update the input field with the formatted name
-  }document.getElementById('user_name').addEventListener('input', formatName);
+  }nameInput.addEventListener('input', formatName);
   
   // Function to format the vist reason input
   function formatVisitReason(){
-    const visitReasonInput = document.getElementById('visit-reason');
     let visitReason = visitReasonInput.value.replace(/[^a-zA-Z1-9 ]/g, ""); // Remove non-alphabetic characters
     visitReasonInput.value = visitReason; // Update the input field with the formatted name
-  }document.getElementById('visit-reason').addEventListener('input', formatVisitReason);
+  }visitReasonInput.addEventListener('input', formatVisitReason);
 
   // Function to format the vist company input
   function formatVisitCompany(){
-    const formatVisitCompanyInput = document.getElementById('visit-company');
     let visitCompany = formatVisitCompanyInput.value.replace(/[^a-zA-Z1-9 ]/g, ""); // Remove non-alphabetic characters and non-numerical characters 
     formatVisitCompanyInput.value = visitCompany; // Update the input field with the formatted name
-  }document.getElementById('visit-company').addEventListener('input', formatVisitCompany);
+  }formatVisitCompanyInput.addEventListener('input', formatVisitCompany);
 
   // Function to format the contractor visit input
   function formatContractorReason(){
-    const contractorReasonInput = document.getElementById('contractor-reason');
     let contractorReason = contractorReasonInput.value.replace(/[^a-zA-Z1-9 ]/g, ""); // Remove non-alphabetic characters
     contractorReasonInput.value = contractorReason; // Update the input field with the formatted name
-  }document.getElementById('contractor-reason').addEventListener('input', formatContractorReason);
+  }contractorReasonInput.addEventListener('input', formatContractorReason);
 
   // Function to format the contractor company input
   function formatContractorCompany(){
-    const contractorCompanyInput = document.getElementById('contractor-company');
     let contractorCompany = contractorCompanyInput.value.replace(/[^a-zA-Z1-9 ]/g, ""); // Remove non-alphabetic characters
     contractorCompanyInput.value = contractorCompany; // Update the input field with the formatted name
-  }document.getElementById('contractor-company').addEventListener('input', formatContractorCompany);
+  }contractorCompanyInput.addEventListener('input', formatContractorCompany);
 
   // Function to format the contractor contact input
   function formatContractorContact(){
-    const contractorContactInput = document.getElementById('contractor-contact');
     let contractorContact = contractorContactInput.value.replace(/[^a-zA-Z1-9 ]/g, ""); // Remove non-alphabetic characters
     contractorContactInput.value = contractorContact; // Update the input field with the formatted name
-  }document.getElementById('contractor-contact').addEventListener('input', formatContractorContact);
+  }contractorContactInput.addEventListener('input', formatContractorContact);
 
   // Function to format the contractor area input
   function formatContractorArea(){
-    const contractorAreaInput = document.getElementById('contractor-area');
     let contractorArea = contractorAreaInput.value.replace(/[^a-zA-Z1-9 ]/g, ""); // Remove non-alphabetic characters
     contractorAreaInput.value = contractorArea; // Update the input field with the formatted name
-  }document.getElementById('contractor-area').addEventListener('input', formatContractorArea);
-
-  // Function to format the original contractor names input
-  function formatAdditionalContractorNames(){
-    const contractorNamesInput = document.getElementById('additional-contractor-names');
-    let contractorNames = contractorNamesInput.value.replace(/[^a-zA-Z ]/g, ""); // Remove non-alphabetic characters
-    contractorNamesInput.value = contractorNames; // Update the input field with the formatted name
-  }document.getElementById('additional-contractor-names').addEventListener('input', formatAdditionalContractorNames);
+  }contractorAreaInput.addEventListener('input', formatContractorArea);
